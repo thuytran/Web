c0271101464e30d37bf8bd8be7bae8731a34966c
Thuy Tran <thuyexpress@gmail.com>
2013-03-18 22:28:21 +0700

    init commit

---
+++ application/views/myview.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">  
+<html xml:lang="en-us" xmlns="http://www.w3.org/1999/xhtml">  
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<link href="http://localhost/Web/assets/css/style.css" media="screen" rel="stylesheet" type="text/css" /> 
+		<title>Chào mừng bạn đến với trang web tin tức công nghệ</title>
+		
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+				<div>
+					<a href="http://localhost/Web">
+						<img src="assets/img/logo.gif"  alt="NewTech"/>
+					</a>
+				</div>
+				<ul>
+				<li class="current"><a href="http://localhost/Web"><span>Home</span></a></li>
+				<li><a href="TinICT.php"><span>Tin ICT</span></a></li>
+				<li><a href="Mobile.php"><span>Mobile</span></a></li>
+				<li><a href="Lap.php"><span>Laptop &amp; Tablet</span></a></li>
+				<li><a href="Pc.php"><span>PC</span></a></li>
+				<li><a href="KhamPha.php"><span>Khám Phá</span></a></li>
+			</ul>
+			</div>
+			<div id="body">
+			
+				<ul>
+					<li>
+						<h1><a href="TinICT.php">Tin ICT</a></h1>
+					<div>
+						<a href="TinICT.php"><img src="assets/img/ImageView.png" alt="Image" /></a>
+					</div>
+					<span> Đây là bài mục ICT</span>
+					<a href="TinICT.php" class="readmore">Read more</a>
+					</li>
+					
+					<li>
+						<h1><a href="Mobile.php">Mobile</a></h1>
+					<div>
+						<a href="Mobile.php"><img src="assets/img/mobile.png" alt="Image" /></a>
+					</div>
+					<span> Đây là bài mục Mobile</span>
+					<a href="Mobile.php" class="readmore">Read more</a>
+					</li>
+					
+					<li>
+						<h1><a href="Lap.php">Lap top</a></h1>
+					<div>
+						<a href="Lap.php"><img src="assets/img/lap.png" alt="Image" /></a>
+					</div>
+					<span> Đây là bài mục Laptop</span>
+					<a href="Lap.php" class="readmore">Read more</a>
+					</li>
+					
+					<li>
+						<h1><a href="Pc.php">PC</a></h1>
+					<div>
+						<a href="Pc.php"><img src="assets/img/pc.png" alt="Image" /></a>
+					</div>
+					<span> Đây là bài mục PC</span>
+					<a href="Pc.php" class="readmore">Read more</a>
+					</li>
+					
+					<li>
+						<h1><a href="KhamPha.php">Khám phá</a></h1>
+					<div>
+						<a href="KhamPha.php"><img src="assets/img/khampha.png" alt="Image" /></a>
+					</div>
+					<span> Đây là bài mục Khám phá</span>
+					<a href="KhamPha.php" class="readmore">Read more</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		
+	</body>
+</html>
